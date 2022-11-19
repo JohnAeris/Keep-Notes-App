f181f2eec0927b0b70112c58bdd19de26fedc29a
johnaeris <barrotjohnaeris@gmail.com>
2022-11-19 17:58:00 +0800

    Add button component

---
+++ app/src/main/java/com/example/keepnotes/components/Button.kt
@@ -0,0 +1,57 @@
+package com.example.keepnotes.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SaveButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    enabled: Boolean = true
+) {
+
+    Button(
+        onClick = onClick,
+        shape = CircleShape,
+        enabled = enabled,
+        border = BorderStroke(1.dp, MaterialTheme.colors.primary),
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary),
+        modifier = Modifier.width(100.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.button,
+            color = MaterialTheme.colors.primaryVariant
+        )
+    }
+}
+
+@Composable
+fun DiscardButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    enabled: Boolean = true
+) {
+
+    Button(
+        onClick = onClick,
+        shape = CircleShape,
+        enabled = enabled,
+        border = BorderStroke(1.dp, MaterialTheme.colors.secondary),
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.background),
+        modifier = Modifier.width(100.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.button,
+            color = MaterialTheme.colors.primary
+        )
+    }
+}
