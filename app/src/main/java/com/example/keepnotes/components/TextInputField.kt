f2ec493753662a7a1ab2052e5247f66f6e60c404
johnaeris <barrotjohnaeris@gmail.com>
2022-11-19 17:58:27 +0800

    Update

--- app/src/main/java/com/example/keepnotes/components/TextInputField.kt
+++ app/src/main/java/com/example/keepnotes/components/TextInputField.kt
@@ -1,18 +1,27 @@
 package com.example.keepnotes.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import com.example.keepnotes.R
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun InputTextField(
+fun InputText(
     modifier: Modifier = Modifier,
     text: String,
     label: String,
@@ -26,7 +35,12 @@ fun InputTextField(
         value = text,
         onValueChange = onTextChange,
         maxLines = maxLine,
-        label = { Text(text = label) },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.background),
+        label = { Text(
+            text = label,
+            fontWeight = FontWeight.Medium,
+            color = MaterialTheme.colors.primary)},
         keyboardOptions = KeyboardOptions.Default.copy(
             imeAction = ImeAction.Done),
         keyboardActions = KeyboardActions(
