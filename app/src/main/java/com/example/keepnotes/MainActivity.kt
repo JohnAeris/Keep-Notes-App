e9e7cedb85b6b5b775fdaa46fbcdc3567788c0e0
johnaeris <barrotjohnaeris@gmail.com>
2022-11-19 16:30:45 +0800

    Modify

--- app/src/main/java/com/example/keepnotes/MainActivity.kt
+++ app/src/main/java/com/example/keepnotes/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.keepnotes.navigation.ScreenNavigation
+import com.example.keepnotes.screen.MainScreen
 import com.example.keepnotes.ui.theme.KeepNotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    ScreenNavigation()
                 }
             }
         }
