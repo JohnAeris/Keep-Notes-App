02fa3a0ef876a6930f7b62353c1f18bc903125bb
johnaeris <barrotjohnaeris@gmail.com>
2022-11-19 14:46:44 +0800

    Add cuztomize typography

--- app/src/main/java/com/example/keepnotes/ui/theme/Type.kt
+++ app/src/main/java/com/example/keepnotes/ui/theme/Type.kt
@@ -2,27 +2,51 @@ package com.example.keepnotes.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.keepnotes.R
 
 // Set of Material typography styles to start with
+
+val Sono = FontFamily(Font(R.font.sono))
+val RobotoBold = FontFamily(Font(R.font.roboto_condensed_bold))
+val RobotoBoldItalic = FontFamily(Font(R.font.roboto_condensed_bold_italic))
+val RobotoNormal = FontFamily(Font(R.font.roboto_condensed_regular))
+val RobotoItalic = FontFamily(Font(R.font.roboto_condensed_italic))
+val RobotoLight = FontFamily(Font(R.font.roboto_condensed_light))
+val RobotoLightItalic = FontFamily(Font(R.font.roboto_condensed_light_italic))
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+
+    h1 = TextStyle(
+        fontFamily = RobotoBold,
+        fontWeight = FontWeight.Black,
+        fontSize = 24.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
+
+    h2 = TextStyle(
+        fontFamily = RobotoBold,
+        fontSize = 16.sp
+    ),
+
+    body1 = TextStyle(
+        fontFamily = Sono,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
-    )
-    */
+    ),
+
+    caption = TextStyle(
+        fontFamily = RobotoLightItalic,
+        fontSize = 10.sp
+    ),
+
+    button = TextStyle(
+        fontFamily = RobotoNormal,
+        fontSize = 16.sp
+    ),
+
+
+
 )
