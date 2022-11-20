912003cca3f3a708f542ed7b7aca4fe26ce44846
johnaeris <barrotjohnaeris@gmail.com>
2022-11-20 22:37:59 +0800

    Add mutable state of list

--- app/src/main/java/com/example/keepnotes/navigation/ScreenNavigation.kt
+++ app/src/main/java/com/example/keepnotes/navigation/ScreenNavigation.kt
@@ -1,26 +1,38 @@
 package com.example.keepnotes.navigation
 
-import android.net.http.SslCertificate
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.keepnotes.model.NoteData
 import com.example.keepnotes.screen.AddNoteScreen
 import com.example.keepnotes.screen.MainScreen
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun ScreenNavigation() {
     val navController = rememberNavController()
+    val noteList = remember { mutableStateListOf<NoteData>() }
 
     NavHost(navController = navController, startDestination = Screen.MainScreen.name) {
 
-        composable(Screen.MainScreen.name) { MainScreen(navController) }
+        composable(Screen.MainScreen.name){
+            MainScreen(navController = navController, noteList = noteList, removeNote = { noteList.remove(it) })}
 
-        composable(Screen.AddNoteScreen.name) { AddNoteScreen(navController) }
+        composable(Screen.AddNoteScreen.name){
+
+
+
+            AddNoteScreen(navController = navController, addNote = { noteList.add(it) })
+        }
 
     }
 }
