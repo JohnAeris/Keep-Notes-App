a6cd4470d80ffff852756736d33d06a4747a312a
johnaeris <barrotjohnaeris@gmail.com>
2022-11-20 22:39:23 +0800

    Add remove button

--- app/src/main/java/com/example/keepnotes/screen/MainScreen.kt
+++ app/src/main/java/com/example/keepnotes/screen/MainScreen.kt
@@ -3,34 +3,42 @@ package com.example.keepnotes.screen
 import android.os.Build
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.keepnotes.data.NoteDummyDataSource
 import com.example.keepnotes.model.NoteData
 import com.example.keepnotes.navigation.Screen
-import java.text.SimpleDateFormat
-import java.util.*
+import java.time.format.DateTimeFormatter
 
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun MainScreen(navController: NavController) {
+fun MainScreen(
+    navController: NavController,
+    noteList: List<NoteData>,
+    removeNote: (NoteData) -> Unit
+) {
     val context = LocalContext.current
-
     Scaffold(
 
         topBar = {
@@ -65,22 +73,86 @@ fun MainScreen(navController: NavController) {
 
         floatingActionButtonPosition = FabPosition.Center)
     {
-        MainContent(notes = NoteDummyDataSource().loadNotes(), addNote = {}, removeNote = {})
+        MainContent(notes = noteList, onRemoved = removeNote)
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun MainContent(notes: List<NoteData>, addNote: (NoteData) -> Unit, removeNote: (NoteData) -> Unit) {
+fun MainContent(notes: List<NoteData>, onRemoved: (NoteData) -> Unit) {
 
-    LazyColumn() {
-        item() {
-            Text(text = "HELLO", color = Color.Black)
-        }
+    LazyColumn(modifier = Modifier.background(color = MaterialTheme.colors.primaryVariant)) {
 
         items(notes) { note ->
-            Text(text = note.title, color = Color.Black)
+            NoteCard(note = note, onRemoved = { onRemoved(note) })
         }
 
     }
 
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun NoteCard(note: NoteData, onRemoved: (NoteData) -> Unit) {
+
+    val context = LocalContext.current
+
+    Card(
+        backgroundColor = MaterialTheme.colors.background,
+        contentColor = MaterialTheme.colors.primary,
+        shape = RoundedCornerShape(CornerSize(8.dp)),
+        modifier = Modifier
+            .padding(start = 15.dp, end = 15.dp, top = 10.dp, bottom = 10.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()) {
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+            modifier = Modifier.padding(10.dp)) {
+
+            Row() {
+                Text(
+                    text = note.title,
+                    style = MaterialTheme.typography.h2,
+                    color = MaterialTheme.colors.primary,
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.weight(1f)
+                )
+
+                Column(horizontalAlignment = Alignment.End) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = "Delete Button",
+                        modifier = Modifier
+                            .size(20.dp)
+                            .clickable {
+                                onRemoved(note)
+                                Toast.makeText(context, "Remove", Toast.LENGTH_SHORT).show()
+                            }
+                    )
+                }
+            }
+
+            Column(horizontalAlignment = Alignment.Start) {
+                Text(
+                    text = note.description,
+                    style = MaterialTheme.typography.body1,
+                    color = MaterialTheme.colors.primary,
+                    textAlign = TextAlign.Justify
+                )
+            }
+            
+            Row() {
+                Spacer(modifier = Modifier.weight(1f))
+                
+                Text(
+                    text = note.date.format(DateTimeFormatter.ofPattern("hh:mm a | d MMM, y")),
+                    style = MaterialTheme.typography.caption,
+                    color = MaterialTheme.colors.primary)   
+            }
+
+        }
+
+    }
+    
 }
