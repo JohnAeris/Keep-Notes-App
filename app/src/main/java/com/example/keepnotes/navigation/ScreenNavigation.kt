726ecb5f6dfb7dbc003363e9f726f786017c6ccb
johnaeris <barrotjohnaeris@gmail.com>
2022-11-20 23:22:55 +0800

    Modify utilize viewModel

--- app/src/main/java/com/example/keepnotes/navigation/ScreenNavigation.kt
+++ app/src/main/java/com/example/keepnotes/navigation/ScreenNavigation.kt
@@ -15,23 +15,25 @@ import com.example.keepnotes.screen.AddNoteScreen
 import com.example.keepnotes.screen.MainScreen
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.keepnotes.viewModel.NoteViewModel
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun ScreenNavigation() {
+fun ScreenNavigation(noteViewModel: NoteViewModel = viewModel()) {
+    val noteList = noteViewModel.getAllNotes()
     val navController = rememberNavController()
-    val noteList = remember { mutableStateListOf<NoteData>() }
 
     NavHost(navController = navController, startDestination = Screen.MainScreen.name) {
 
         composable(Screen.MainScreen.name){
-            MainScreen(navController = navController, noteList = noteList, removeNote = { noteList.remove(it) })}
+            MainScreen(navController = navController, noteList = noteList, removeNote = { noteViewModel.removeNote(it) })}
 
         composable(Screen.AddNoteScreen.name){
 
 
 
-            AddNoteScreen(navController = navController, addNote = { noteList.add(it) })
+            AddNoteScreen(navController = navController, addNote = { noteViewModel.addNote(it) })
         }
 
     }
