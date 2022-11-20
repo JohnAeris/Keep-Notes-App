348764eabe7d0aa609a56f6bb8095bee5631be6b
johnaeris <barrotjohnaeris@gmail.com>
2022-11-20 20:23:33 +0800

    Add text styles

--- app/src/main/java/com/example/keepnotes/components/TextInputField.kt
+++ app/src/main/java/com/example/keepnotes/components/TextInputField.kt
@@ -4,14 +4,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -33,6 +31,7 @@ fun InputText(
     
     TextField(
         value = text,
+        textStyle = MaterialTheme.typography.body1,
         onValueChange = onTextChange,
         maxLines = maxLine,
         colors = TextFieldDefaults.textFieldColors(
