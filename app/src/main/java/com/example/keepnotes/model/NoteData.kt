f4f1464da1410db0bdeb867e4900d6f7950d6d76
johnaeris <barrotjohnaeris@gmail.com>
2022-11-22 17:05:23 +0800

    Modify date

--- app/src/main/java/com/example/keepnotes/model/NoteData.kt
+++ app/src/main/java/com/example/keepnotes/model/NoteData.kt
@@ -2,14 +2,28 @@ package com.example.keepnotes.model
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import java.time.Instant
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
 @RequiresApi(Build.VERSION_CODES.O)
+@Entity(tableName = "KeepNotesTable")
 data class NoteData (
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "noteTitle")
     val title: String,
+
+    @ColumnInfo(name = "noteDescription")
     val description: String,
-    val date: LocalDateTime = LocalDateTime.now()
+
+    @ColumnInfo(name = "noteDate")
+    val date: Date = Date.from(Instant.now())
 )
