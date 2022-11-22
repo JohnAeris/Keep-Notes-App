a1cd52900d4c42562b4b2452019349d87c759d91
johnaeris <barrotjohnaeris@gmail.com>
2022-11-22 17:03:43 +0800

    Create DAO

---
+++ app/src/main/java/com/example/keepnotes/data/NoteDatabaseDao.kt
@@ -0,0 +1,34 @@
+package com.example.keepnotes.data
+
+import android.provider.ContactsContract.CommonDataKinds.Note
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.keepnotes.model.NoteData
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDatabaseDao {
+
+    @Query("SELECT * FROM KeepNotesTable")
+    fun getAllNotes(): Flow<List<NoteData>>
+
+    @Query("SELECT * FROM KeepNotesTable WHERE id =:id")
+    suspend fun getNoteById(id: String): NoteData
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: NoteData)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateNote(note: NoteData)
+
+    @Query("DELETE FROM KeepNotesTable")
+    suspend fun deleteAllNote()
+
+    @Delete
+    suspend fun deleteNote(note: NoteData)
+
+}
