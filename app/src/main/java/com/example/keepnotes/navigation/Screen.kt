d9ecad46354bcfad222d949570436b20e9342236
johnaeris <barrotjohnaeris@gmail.com>
2022-11-23 22:05:35 +0800

    Add update note screen route

--- app/src/main/java/com/example/keepnotes/navigation/Screen.kt
+++ app/src/main/java/com/example/keepnotes/navigation/Screen.kt
@@ -2,12 +2,14 @@ package com.example.keepnotes.navigation
 
 enum class Screen {
     MainScreen,
-    AddNoteScreen;
+    AddNoteScreen,
+    UpdateNoteScreen;
     companion object {
         fun appRoute(route: String?): Screen
         = when (route?.substringBefore("/")) {
             MainScreen.name -> MainScreen
             AddNoteScreen.name -> AddNoteScreen
+            UpdateNoteScreen.name -> UpdateNoteScreen
             null -> MainScreen
             else -> throw IllegalArgumentException("Route $route doesn't exist!")
         }
