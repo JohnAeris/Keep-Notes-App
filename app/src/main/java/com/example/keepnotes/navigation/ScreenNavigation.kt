ae05fec80dddda7b06eb60cf69ed19e17d486640
johnaeris <barrotjohnaeris@gmail.com>
2022-11-23 22:06:00 +0800

    Add passing argument

--- app/src/main/java/com/example/keepnotes/navigation/ScreenNavigation.kt
+++ app/src/main/java/com/example/keepnotes/navigation/ScreenNavigation.kt
@@ -3,15 +3,15 @@ package com.example.keepnotes.navigation
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.*
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.example.keepnotes.model.NoteData
 import com.example.keepnotes.screen.AddNoteScreen
 import com.example.keepnotes.screen.MainScreen
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.example.keepnotes.screen.UpdateNoteScreen
 import com.example.keepnotes.viewModel.NoteViewModel
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -27,6 +27,19 @@ fun ScreenNavigation(noteViewModel: NoteViewModel = viewModel()) {
 
         composable(Screen.AddNoteScreen.name){
             AddNoteScreen(navController = navController, addNote = { noteViewModel.addNote(it) })
+        }
+
+        composable(Screen.UpdateNoteScreen.name + "/{noteId}",
+            arguments = listOf(navArgument(name = "noteId") {type = NavType.StringType})
+            ) { backStackEntry ->
+                UpdateNoteScreen(
+                    noteList = noteList,
+                    navController = navController,
+                    updateNote = { noteViewModel.updateNote(it) },
+                    /*getNoteId = { noteViewModel.getNoteById(it.id) },*/
+                    NoteId = backStackEntry.arguments?.getString("noteId")
+                )
+
         }
 
     }
